aeadc809cb55127da9296301701a9c6681c2c331
Koneru <sai.arjun.koneru@sap.com>
2024-04-16 16:52:29 +0200

    Demo Script to query LLM for a fun fact when fun keyword is said

--- butler.py
+++ butler.py
@@ -8,5 +8,13 @@ class Butler:
         self.client = InferenceClient(model=llm_url)
 
     def process(self, new_message):
-        logging.info("Recieved New Message from ASR:\n " + new_message)
+        print(new_message)
         
+        # Process based on your dialog logic here
+
+        # This is a simple bot that replies with a fun fact whenever you say fun
+
+        if 'fun' in new_message.lower():
+            print("Asking Llama for a fun fact")
+            fun_fact = self.client.text_generation(prompt="<s>[INST] Hello, please tell me a fun fact about Germany in one short line? [/INST]", do_sample=True, max_new_tokens=64)
+            print(fun_fact)
