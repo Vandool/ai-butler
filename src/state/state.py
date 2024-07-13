e0e184ed76ae38302e00dc870a84f27a04f5bd42
namid <namid.marxen@gmail.com>
2024-07-13 13:09:12 +0200

    Fixed test cases

--- src/state/state.py
+++ src/state/state.py
@@ -186,7 +186,7 @@ class InitialState(State):
                     llm_client=self.llm_client,
                     tts_client=self.tts_client,
                     history=self.history,
-                    module=LectureTranslatorAPI,
+                    module=LectureTranslatorApi,
                 ).process(
                     user_input,
                 )
